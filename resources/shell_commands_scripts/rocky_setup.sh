59b27cfcf171b12b4da113b3b5a1a3f5e1568caf
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-08 18:08:24 +0200

    add new files

---
+++ resources/shell_commands_scripts/rocky_setup.sh
@@ -0,0 +1,12 @@
+dnf update
+dnf upgrade
+dnf install -y findutils
+dnf install -y git
+dnf install -y vim
+dnf install -y ncurses
+dnf install -y epel-release
+dnf install -y htop
+dnf install -y tree
+dnf install -y wget
+dnf install -y make gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel
+dnf install net-tools
