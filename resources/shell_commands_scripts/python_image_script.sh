ac503fe1b64322edd53ffc8c56a2d723c0b9e7f5
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-06-09 06:58:47 +0300

    add new scripts

---
+++ resources/shell_commands_scripts/python_image_script.sh
@@ -0,0 +1,8 @@
+apt update
+apt upgrade
+apt-get install inetutils-ping
+apt-get install net-tools
+apt install vim
+apt-get install openssh-client
+python -m pip install --upgrade pip
+apt-get install mosquitto-clients
---
+++ resources/shell_commands_scripts/rocky_image_script.sh
@@ -0,0 +1,9 @@
+dnf update
+dnf upgrade
+dnf install epel-release
+dnf install net-tools
+dnf install ncurses
+dnf install vim
+dnf install openssh-client
+dnf install iputils
+dnf install mosquitto
